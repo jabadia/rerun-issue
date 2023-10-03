1e8fd3f65bf61cfe6ef9a111af98bdf789bb944a
Javier Abadía <javi.abadia@gmail.com>
2023-10-03 09:24:04 +0200

    reproducing issue

---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+import logging
+import streamlit as st
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='$(threadid)d %(asctime)s %(levelname)s %(message)s')
+
+logger.info(f'starting cycle with state {st.session_state}')
+
+st.title('playground')
+
+tab1, tab2 = st.tabs(['Tab1', 'Tab2'])
+
+with tab1:
+    form1 = st.form(key='form1')
+    form1.selectbox(
+        label='Pick a City',
+        options=['Madrid', 'Barcelona', 'Sevilla', 'Valencia', 'Bilbao', 'Zaragoza'],
+        key='selected_city'
+    )
+    submit_btn = form1.form_submit_button(label='Submit')
+
+    if submit_btn:
+        logger.info(f'submitting form1 {st.session_state}')
+        time.sleep(2)
+        logger.info(f'submitted form1 {st.session_state}')
+        st.write(f'form1 submitted: {st.session_state.selected_city}')
+    else:
+        st.write(f'form1 not submitted')
+
+with tab2:
+    image_url = st.text_input(
+        label='Image URL',
+        key='image_url'
+    )
+
+    st.file_uploader(
+        label='Or Upload your own image',
+        key='image_uploader',
+        disabled=bool(image_url)
+    )
+
+logger.info('cycle finished')
